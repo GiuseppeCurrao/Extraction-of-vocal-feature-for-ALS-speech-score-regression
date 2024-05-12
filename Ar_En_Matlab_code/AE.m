63e0821a5d29844a4cd4a8083112098cf7119b18
GiuseppeCurrao <uni400704@studium.unict.it>
2024-05-12 10:57:54 +0200

    Create AE.m

---
+++ Ar_En_Matlab_code/AE.m
@@ -0,0 +1,23 @@
+clear all;
+close all;
+clc;
+%%
+% You may need to add voicebox (a Matlab toolbox) into your path
+code_folder =pwd;
+path_HC = fullfile(code_folder, "Data\Healthy Control\Normal");
+csv_HC = readtable("Healthy Control\Normal.csv");
+%%
+for i = 1:size(csv_HC,1)
+    file_path=fullfile(path_HC, csv_HC{i,1});
+    [y,fs] = audioread(file_path);
+    
+end
+
+[y,fs] = audioread('the path to your speech sample');
+vad = vadsohn(y,fs);
+y = y(vad==1);
+y_nor = inten_norm(y,fs);
+F = melroot3_extraction(y_nor,16000);
+ae = artic_ent(F,200); % Set the second parameter based on the length of 
+                       % your data or the minimum length of the data in
+                       % your dataset.
